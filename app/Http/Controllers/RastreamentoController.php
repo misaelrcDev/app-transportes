239e1a2d53c53ce7644d916421ae06a60a9efed6
Misael Cabral <misaelrc.dev@gmail.com>
2025-05-24 09:36:38 -0300

    Criado as Models

--- app/Http/Controllers/RastreamentoController.php
+++ app/Http/Controllers/RastreamentoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Frete;
 use Illuminate\Http\Request;
 
 class RastreamentoController extends Controller
@@ -11,6 +12,10 @@ class RastreamentoController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $frete = Frete::get();
+
+        dd($frete);
+
         return view('fretes.rastreamento');
     }
 }
